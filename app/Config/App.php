17e99110e7e06d97d33eeba11b7ba97de47bf8bc
azhariazna <30310837+azhariazna@users.noreply.github.com>
2025-06-14 19:15:09 +0800

    14

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,17 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost:8080/';
+    /**public string $baseURL = 'http://localhost:8080/';**/
+	public string $baseURL = '';
+
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->baseURL = (isset($_SERVER['HTTPS']) ? "https://" : "http://")
+					   . $_SERVER['HTTP_HOST']
+					   . str_replace('/index.php', '', $_SERVER['SCRIPT_NAME']) . '/';
+	}
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
