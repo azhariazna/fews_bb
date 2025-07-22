7ab3d1c0bf631c53f0c4ede41a20c7474ed562d7
Azhari Azna <azna39@gmail.com>
2025-07-22 11:51:42 +0800

    ok

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -44,6 +44,13 @@
             </a>
           </li>
 
+          <li class="nav-item">
+            <a href="<?php echo base_url('/')?>" class="nav-link">
+              <i class="nav-icon fas fa-map"></i>
+              <p>Dashboard Peta</p>
+            </a>
+          </li>
+
             <!-- Dropdown Menu RTD -->
           <li class="nav-item has-treeview">
             <a href="#" class="nav-link">
@@ -69,14 +76,6 @@
             </ul>
           </li>
 
-
-
-          <li class="nav-item">
-            <a href="<?php echo base_url('/')?>" class="nav-link">
-              <i class="nav-icon fas fa-map"></i>
-              <p>Dashboard Peta</p>
-            </a>
-          </li>
           <li class="nav-item">
             <a href="manual-tma" class="nav-link" target="content-frame">
               <i class="nav-icon fas fa-keyboard"></i>
