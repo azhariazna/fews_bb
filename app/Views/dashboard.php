aa3c777e11da90d21610878085fd79af3d81ece6
Azhari Azna <azna39@gmail.com>
2025-08-04 15:13:00 +0800

    ok

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -3,6 +3,8 @@
 <head>
   <meta charset="UTF-8">
   <title>Dashboard</title>
+  <link rel="icon" type="image/png" href="https://sda.pu.go.id/balai/bbwsnt1/assets/img/favicon.png">
+
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free/css/all.min.css">
